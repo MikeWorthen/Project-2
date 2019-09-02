ae52da9a05b6ebc23c1e62f7109d18426ca517eb
Matt <mattsully01@gmail.com>
2019-09-01 18:32:18 -0600

    commiting path changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,7 @@
 var db = require("../models");
+const Sequalize = require("sequelize");
+const Op = Sequalize.Op;
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
@@ -29,7 +32,19 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/profile/:search?", (req, res) => {
+    let { user } = req.query;
 
+    db.Bitmaps.findOne({
+      where: { username: { [op.like]: '%'+ user + '%' } }})
+      .then(function(dbProject2) {
+      res.render("profile", {
+        title: "Bitmap - My Profile",
+        msg: "Welcome!",
+        user: dbProject2
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
